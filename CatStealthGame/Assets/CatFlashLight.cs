c519e2849acc124e25678c05b2ea0deaae1c3fb2
Nathan Montoya <montoya.nlm@gmail.com>
2026-03-21 23:09:19 -0600

    Revert "Test"
    This reverts commit 75ebeebf856761ac37eef4f806309f169c8c553c.

--- CatStealthGame/Assets/CatFlashLight.cs
+++ CatStealthGame/Assets/CatFlashLight.cs
@@ -1,21 +1,16 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CatFlashLight : MonoBehaviour
 {
-    public Quaternion dir;
-    public Transform head;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        head = GetComponentInParent<CatAimController>().headPivot;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        dir = (Quaternion)head.rotation;
-        transform.rotation = dir;
-        transform.position = head.position;
+        
     }
 }
