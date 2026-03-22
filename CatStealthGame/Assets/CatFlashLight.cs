75ebeebf856761ac37eef4f806309f169c8c553c
Nathan Montoya <montoya.nlm@gmail.com>
2026-03-21 23:07:31 -0600

    Test

--- CatStealthGame/Assets/CatFlashLight.cs
+++ CatStealthGame/Assets/CatFlashLight.cs
@@ -1,16 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CatFlashLight : MonoBehaviour
 {
+    public Quaternion dir;
+    public Transform head;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        head = GetComponentInParent<CatAimController>().headPivot;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        dir = (Quaternion)head.rotation;
+        transform.rotation = dir;
+        transform.position = head.position;
     }
 }
